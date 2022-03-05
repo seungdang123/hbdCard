f97738568cbb14b4f2f84c0780aa913a55006c35
seungdang123 <seungdang123@gmail.com>
2022-03-05 23:17:56 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import Logo from "./components/Logo";
+
+const Wrapper = styled(motion.div)`
+  background: rgb(215, 103, 245);
+  background: linear-gradient(
+    9deg,
+    rgba(215, 103, 245, 1) 0%,
+    rgba(255, 181, 248, 1) 52%,
+    rgba(255, 216, 146, 1) 100%
+  );
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Logo />
+    </Wrapper>
   );
 }
 
